e77363a39980ea47a3e1ddafc8941f9b110c7a25
roden1999 <rodenmontoya5@gmail.com>
2026-01-11 03:28:58 +0800

    all queries convert into stored procedure

--- app/api/models/transactions.ts
+++ app/api/models/transactions.ts
@@ -13,109 +13,91 @@ export interface Transactions {
     IsDeleted: boolean;
 }
 
-// Add transactions
-export async function addTransactions(transac: Omit<Transactions, 'Id' | 'IsDeleted'>): Promise<void> {
-    const pool = await connectToDB();
-    await pool
-        .request()
-        .input('User_Id', transac.User_Id)
-        .input('Wallet_Id', transac.Wallet_Id)
-        .input('Amount', transac.Amount)
-        .input('Type', transac.Type)
-        .input('Description', transac.Description)
-        .input('Category', transac.Category)
-        .input('Date', transac.Date)
-        .query(
-            `INSERT INTO Transactions (User_Id, Wallet_Id, Amount, Type, Description, Category, Date, IsDeleted)
-       VALUES (@User_Id, @Wallet_Id, @Amount, @Type, @Description, @Category, @Date, 0)`
-        );
+interface SearchedData {
+    Wallet_Ids?: number[];
+    Type: string;
+    From_Date: Date;
+    To_Date: Date;
 }
 
-// Options for reactselect
-export async function optionsTransactions(Id: number) {
+// Add transactions
+export async function addTransactions(transac: Omit<Transactions, 'Id' | 'IsDeleted'>) {
     const pool = await connectToDB();
     const result = await pool
         .request()
-        .input('Id', Id)
-        .query<Transactions>(
-            `SELECT * FROM Transactions WHERE User_Id = @Id AND IsDeleted = 0`
-        );
+        .input('json_data', JSON.stringify(transac))
+        .execute('[post_transaction]');
 
+    return result.recordset;
+}
+
+export async function listTransactions(data: SearchedData) {
+    const pool = await connectToDB();
+    const result = await pool
+        .request()
+        .input('json_data', JSON.stringify(data))
+        .execute('[get_transactions]');
+        console.log("Debug", result.recordset);
     return result.recordset;
 }
 
 // List all transactions
-export async function listAllTransactions(Id: number) {
-    const pool = await connectToDB();
-    const result = await pool
-        .request()
-        .input('Id', Id)
-        .query<Transactions>(`
-            SELECT t.*, w.Name AS Wallet
-            FROM Transactions t
-            LEFT JOIN Wallets w ON t.Wallet_Id = w.Id
-            WHERE t.User_Id = @Id AND t.IsDeleted = 0
-            ORDER BY t.Date DESC
-        `);
-    return result.recordset;
-}
+// export async function listAllTransactions(Id: number) {
+//     const pool = await connectToDB();
+//     const result = await pool
+//         .request()
+//         .input('Id', Id)
+//         .query<Transactions>(`
+//             SELECT t.*, w.Name AS Wallet
+//             FROM Transactions t
+//             LEFT JOIN Wallets w ON t.Wallet_Id = w.Id AND w.IsDeleted = 0 -- onlyjoin active wallte
+//             WHERE t.User_Id = @Id AND t.IsDeleted = 0
+//             AND w.Id IS NOT NULL --wallet exist
+//             ORDER BY t.Date DESC
+//         `);
+//     return result.recordset;
+// }
 
 // List searched transactions
-export async function listSearchedTransactions(Id: number, Type: string, FromDate: Date, ToDate: Date) {
-    const pool = await connectToDB();
-    const request = pool.request();
-    let query = `
-        SELECT t.*, w.Name AS Wallet
-        FROM Transactions t
-        LEFT JOIN Wallets w ON t.Wallet_Id = w.Id
-        WHERE t.User_Id = @User_Id AND t.IsDeleted = 0
-    `;
-    request.input('User_Id', Id);
+// export async function listSearchedTransactions(Id: number, Type: string, FromDate: Date, ToDate: Date) {
+//     const pool = await connectToDB();
+//     const request = pool.request();
+//     let query = `
+//         SELECT t.*, w.Name AS Wallet
+//         FROM Transactions t
+//         LEFT JOIN Wallets w ON t.Wallet_Id = w.Id
+//         WHERE t.User_Id = @User_Id AND t.IsDeleted = 0
+//     `;
+//     request.input('User_Id', Id);
 
-    if (Type) {
-        query += ' AND Type = @Type';
-        request.input('Type', Type);
-    }
+//     if (Type) {
+//         query += ' AND Type = @Type';
+//         request.input('Type', Type);
+//     }
 
-    if (FromDate) {
-        query += ' AND Date >= @FromDate';
-        request.input('FromDate', FromDate);
-    }
+//     if (FromDate) {
+//         query += ' AND Date >= @FromDate';
+//         request.input('FromDate', FromDate);
+//     }
 
-    if (ToDate) {
-        query += ' AND Date <= @ToDate';
-        request.input('ToDate', ToDate);
-    }
+//     if (ToDate) {
+//         query += ' AND Date <= @ToDate';
+//         request.input('ToDate', ToDate);
+//     }
 
-    query += ' ORDER BY Date DESC';
+//     query += ' ORDER BY Date DESC';
 
-    const result = await request.query<Transactions>(query);
-    return result.recordset;
-}
+//     const result = await request.query<Transactions>(query);
+//     return result.recordset;
+// }
 
 // Edit Transactions
 export async function editTransactions(transac: Omit<Transactions, 'User_Id' | 'Wallet_Id' | 'IsDeleted'>): Promise<void> {
     const pool = await connectToDB();
     await pool
         .request()
-        .input('Id', transac.Id)
-        .input('Amount', transac.Amount)
-        .input('Type', transac.Type)
-        .input('Description', transac.Description)
-        .input('Category', transac.Category)
-        .input('Date', transac.Date)
-        .query(
-            `UPDATE Transactions
-            SET
-                Amount = @Amount,
-                Type = @Type,
-                Description = @Description,
-                Category = @Category,
-                Date = @Date
-            WHERE Id = @Id
-            AND Wallet_Id = @Wallet_Id
-            AND IsDeleted = 0;`
-        );
+        .input('json_data', JSON.stringify(transac))
+        .execute('[post_transaction]');
 }
 
 // Delete Transatcions
@@ -124,11 +106,5 @@ export async function deleteTransaction(Id: number) {
     await pool
         .request()
         .input('Id', Id)
-        .query(
-            `UPDATE Transactions
-            SET
-                IsDeleted = 1
-            WHERE Id = @Id
-            AND IsDeleted = 0;`
-        );
+        .execute('[delete_transaction]');
 }
