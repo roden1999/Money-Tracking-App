6f3fc874e33b51a58cfc2deb131372882644ea5d
roden1999 <rodenmontoya5@gmail.com>
2026-01-13 06:49:55 +0800

    implement profile editing including change password feature

--- app/api/models/user.ts
+++ app/api/models/user.ts
@@ -12,6 +12,39 @@ export interface User {
     IsDeleted: boolean;
 }
 
+// Create a new user
+export async function createUser(data: Omit<User, 'Id' | 'IsDeleted'>) {
+    const pool = await connectToDB();
+    await pool
+        .request()
+        .input('json_data', JSON.stringify(data))
+        .execute('[post_user]');
+}
+
+// Update a new user
+export async function updateUser(data: Omit<User, 'IsDeleted'>): Promise<User | null> {
+    const pool = await connectToDB();
+    const result = await pool
+        .request()
+        .input('json_data', JSON.stringify(data))
+        .execute('[post_user]');
+
+    return result.recordset[0];
+}
+
+//Check username by Id
+export async function findUserById(Id: number): Promise<User | null> {
+    const pool = await connectToDB();
+    const result = await pool
+        .request()
+        .input("Id", Id)
+        .query<User>(
+            `SELECT * FROM Users WHERE Id = @Id AND IsDeleted = 0`
+        );
+
+    return result.recordset[0] || null;
+}
+
 // Check if email or username already exists
 export async function findUserByEmailOrUsername(email: string, username: string): Promise<User | null> {
     const pool = await connectToDB();
@@ -26,11 +59,14 @@ export async function findUserByEmailOrUsername(email: string, username: string)
     return result.recordset[0] || null;
 }
 
-// Create a new user
-export async function createUser(data: Omit<User, 'Id' | 'IsDeleted'>) {
+// Check if email or username already exists
+export async function changePassword(Id: number, password: string): Promise<void> {
     const pool = await connectToDB();
     await pool
         .request()
-        .input('json_data', JSON.stringify(data))
-        .execute('[post_user]');
+        .input('Id', Id)
+        .input('Password', password)
+        .query<User>(
+            `UPDATE Users SET Password = @Password WHERE Id = @Id AND IsDeleted = 0`
+        );
 }
