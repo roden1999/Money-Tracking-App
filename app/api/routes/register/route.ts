58f9bc2ba2dd099279f356908e225fbebe0e267a
roden1999 <rodenmontoya5@gmail.com>
2026-01-13 06:50:26 +0800

    implement profile editing

--- app/api/routes/register/route.ts
+++ app/api/routes/register/route.ts
@@ -1,6 +1,6 @@
 // app/api/routes/register/route.ts
 import { NextRequest } from 'next/server';
-import { registerUserService } from '@/app/api/services/user';
+import { registerUserService, editUserService } from '@/app/api/services/user';
 
 export async function POST(req: NextRequest) {
     try {
@@ -8,6 +8,23 @@ export async function POST(req: NextRequest) {
         await registerUserService(body);
 
         return new Response(JSON.stringify({ message: 'User created successfully' }), {
+            status: 201,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (err: any) {
+        return new Response(JSON.stringify({ message: err.message || 'Server error' }), {
+            status: 400,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+}
+
+export async function PUT(req: NextRequest) {
+    try {
+        const body = await req.json();
+        const result = await editUserService(body);
+        console.log(result);
+        return new Response(JSON.stringify({ result, message: 'User created successfully' }), {
             status: 201,
             headers: { 'Content-Type': 'application/json' },
         });
